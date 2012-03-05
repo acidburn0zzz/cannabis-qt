6e0ba496298bbdc4ff123093421d0443c3f36094
karasu <gustaucastells@gmail.com>
2012-03-05 11:09:53 +0100

    Afegida la classe de databasemanager

---
+++ src/databasemanager.cpp
@@ -0,0 +1,112 @@
+#include "databasemanager.h"
+
+DatabaseManager::DatabaseManager(QObject *parent) :
+    QObject(parent)
+{
+}
+
+DatabaseManager::~DatabaseManager()
+{
+    closeDB();
+}
+
+bool DatabaseManager::openDB()
+{
+    if (!QSqlDatabase::contains())
+    {
+        // Find QSLite driver
+        db = QSqlDatabase::addDatabase("QSQLITE");
+
+        // Set database file name and path
+        QString path(QDesktopServices::storageLocation(QDesktopServices::DataLocation));
+        path.append(QDir::separator()).append("sandra-qt.sqlite3");
+        path = QDir::toNativeSeparators(path);
+        db.setDatabaseName(path);
+
+        qDebug() << "Database file : " + path;
+
+        QFile dbFile(db.databaseName());
+
+        if (!dbFile.exists())
+        {
+            qDebug() << "database does not exist. I'll try to create it.";
+
+            if (!createDB())
+            {
+                return false;
+            }
+        }
+        else
+        {
+            qDebug() << "OK, database already exists.";
+        }
+
+        // Open databasee
+
+        if (!db.open())
+        {
+            qDebug() << db.lastError().text();
+            return false;
+        }
+    }
+    else
+    {
+        db = QSqlDatabase::database();
+    }
+
+    return true;
+}
+
+bool DatabaseManager::createDB()
+{
+    if (!db.open())
+    {
+        qDebug() << db.lastError().text();
+        return false;
+    }
+
+    QFile sqlFile(":/sandra-qt.sql");
+
+    if (sqlFile.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QString sql = "";
+        QTextStream in(&sqlFile);
+        QSqlQuery qry;
+
+        // qry.prepare(in.readAll());
+
+        while (!in.atEnd())
+        {
+           QString line = in.readLine();
+           sql += line;
+
+           if (line.contains(';'))
+           {
+               // debug(sql, __FILE__, __LINE__);
+               if (!qry.exec(sql))
+               {
+                   qDebug() << qry.lastError().text();
+                   db.close();
+                   return false;
+               }
+
+               sql = "";
+           }
+        }
+        sqlFile.close();
+        db.close();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void DatabaseManager::closeDB()
+{
+    if (db.isOpen())
+    {
+        db.close();
+    }
+}
---
+++ src/databasemanager.h
@@ -0,0 +1,39 @@
+#ifndef DATABASEMANAGER_H
+#define DATABASEMANAGER_H
+
+#include <QObject>
+
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+
+#include <QFile>
+#include <QTextStream>
+#include <QDir>
+
+#include <QMessageBox>
+#include <QDebug>
+
+#include <QDesktopServices>
+
+class DatabaseManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DatabaseManager(QObject *parent = 0);
+    ~DatabaseManager();
+
+    bool createDB(void);
+    bool openDB(void);
+    void closeDB(void);
+
+private:
+        QSqlDatabase db;
+
+signals:
+
+public slots:
+
+};
+
+#endif // DATABASEMANAGER_H
