108d943a115c8ae8f37c9b53ca4bcd3bf45950fa
karasu <gustaucastells@gmail.com>
2012-03-06 16:18:50 +0100

    Modificada la base de dades

--- cannabis-qt.sql
+++ cannabis-qt.sql
@@ -1,8 +1,15 @@
 PRAGMA foreign_keys=OFF;
+
 BEGIN TRANSACTION;
-CREATE TABLE "Clients" ( "Codi" INTEGER PRIMARY KEY AUTOINCREMENT, "DNI" TEXT NOT NULL, "Nom" TEXT, "Cognom1" TEXT, "Cognom2" TEXT, "Adr" TEXT, "Telefon" TEXT, "CodiPostal" TEXT, "Poblacio" TEXT);
+
+CREATE TABLE "Clients" ( "Codi" INTEGER PRIMARY KEY AUTOINCREMENT, "DNI" TEXT NOT NULL, "Nom" TEXT, "Cognom1" TEXT, "Cognom2" TEXT, "Adr" TEXT, "Telefon" TEXT, "CodiPostal" TEXT, "Poblacio" TEXT, "email" TEXT);
+
 CREATE TABLE "ConsumCannabis" ( "Id" INTEGER PRIMARY KEY AUTOINCREMENT, "CodiClient" INTEGER, "Data" TEXT, "Grams" TEXT, "Preu" REAL);
-CREATE TABLE "ConsumBar" ( "Id" INTEGER PRIMARY KEY AUTOINCREMENT, "CodiClient" INTEGER, "Data" TEXT, "Producte" TEXT, "Preu" REAL);
+
+CREATE TABLE "Pots" ( "Id" INTEGER PRIMARY KEY AUTOINCREMENT, "Grams" INTEGER);
+
+CREATE TABLE "Caixa" ( "Id" INTEGER PRIMARY KEY AUTOINCREMENT, "Data" TEXT, "Diners" REAL);
+
 COMMIT;
 
 
