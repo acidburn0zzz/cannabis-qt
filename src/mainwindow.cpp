b79182cb7445561d740b01224162df0e61c8c023
karasu <gustaucastells@gmail.com>
2012-04-25 11:37:40 +0200

    Afegida la llicència GPLv3

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,3 +1,21 @@
+/*
+    Cannabis-qt
+    Copyright (C) 2012 Karasu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "mainwindow.h"
 #include "constants.h"
 
@@ -209,7 +227,20 @@ void MainWindow::about()
            "\t- Controlar els ingressos de l'associació i imprimir informes per portar al gestor<br/>"
            "\t- Importació i exportació de les dades (SQLITE)<br/><br/>"
            "Versió %1<br/>"
-           "Fet per Karasu.").arg(PROGRAM_VERSION));
+           "Fet per Karasu.<br /><br />"
+           "Licència:<br />"
+           "Cannabis-qt<br />"
+           "Copyright (C) 2012 Karasu<br /><br />"
+           "This program is free software: you can redistribute it and/or modify<br />"
+           "it under the terms of the GNU General Public License as published by<br />"
+           "the Free Software Foundation, either version 3 of the License, or<br />"
+           "any later version.<br /><br />"
+           "This program is distributed in the hope that it will be useful,<br />"
+           "but WITHOUT ANY WARRANTY; without even the implied warranty of<br />"
+           "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br />"
+           "GNU General Public License for more details.<br /><br />"
+           "You should have received a copy of the GNU General Public License<br />"
+           "along with this program.  If not, see <http://www.gnu.org/licenses/>.").arg(PROGRAM_VERSION));
 }
 
 // ---------------------------------------------------------------------------------------------------------
