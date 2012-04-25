c47475febb59f6675ba7c69799978c53e29350a6
karasu <gustaucastells@gmail.com>
2012-04-25 11:40:48 +0200

    Fixed about dialog

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -231,16 +231,16 @@ void MainWindow::about()
            "Licència:<br />"
            "Cannabis-qt<br />"
            "Copyright (C) 2012 Karasu<br /><br />"
-           "This program is free software: you can redistribute it and/or modify<br />"
-           "it under the terms of the GNU General Public License as published by<br />"
-           "the Free Software Foundation, either version 3 of the License, or<br />"
+           "This program is free software: you can redistribute it and/or modify "
+           "it under the terms of the GNU General Public License as published by "
+           "the Free Software Foundation, either version 3 of the License, or "
            "any later version.<br /><br />"
-           "This program is distributed in the hope that it will be useful,<br />"
-           "but WITHOUT ANY WARRANTY; without even the implied warranty of<br />"
-           "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br />"
+           "This program is distributed in the hope that it will be useful, "
+           "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+           "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
            "GNU General Public License for more details.<br /><br />"
-           "You should have received a copy of the GNU General Public License<br />"
-           "along with this program.  If not, see <http://www.gnu.org/licenses/>.").arg(PROGRAM_VERSION));
+           "You should have received a copy of the GNU General Public License "
+           "along with this program.  If not, see <a href='http://www.gnu.org/licenses/'>www.gnu.org</a>.").arg(PROGRAM_VERSION));
 }
 
 // ---------------------------------------------------------------------------------------------------------
