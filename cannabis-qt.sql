c762586270fae01f2aa6301ec35f9af9ef2c1d8a
karasu <gustaucastells@gmail.com>
2012-05-18 20:05:48 +0200

    Changed sql (boolean is now text)

--- cannabis-qt.sql
+++ cannabis-qt.sql
@@ -1,8 +1,8 @@
-PRAGMA foreign_keys=OFF;
+#PRAGMA foreign_keys=OFF;
 
 BEGIN TRANSACTION;
 
-CREATE TABLE "Socis" ("Codi" INTEGER PRIMARY KEY, "DataAlta" TEXT NOT NULL, "DNI" TEXT NOT NULL, "Nom" TEXT NOT NULL, "Cognom1" TEXT, "Cognom2" TEXT, "Adreça" TEXT, "Telefon" TEXT, "CodiPostal" TEXT, "Poblacio" TEXT, "Email" TEXT, "Pagat" INTEGER);
+CREATE TABLE "Socis" ("Codi" INTEGER PRIMARY KEY, "DataAlta" TEXT NOT NULL, "DNI" TEXT NOT NULL, "Nom" TEXT NOT NULL, "Cognom1" TEXT, "Cognom2" TEXT, "Adreça" TEXT, "Telefon" TEXT, "CodiPostal" TEXT, "Poblacio" TEXT, "Email" TEXT, "Pagat" TEXT);
 
 CREATE TABLE "Cannabis" ("Id" INTEGER PRIMARY KEY AUTOINCREMENT, "CodiSoci" INTEGER NOT NULL, "Data" TEXT NOT NULL, "IdPot" INTEGER NOT NULL, "Grams" INTEGER NOT NULL, "Preu" REAL NOT NULL);
 
@@ -12,10 +12,10 @@ CREATE TABLE "Altres" ("Id" INTEGER PRIMARY KEY AUTOINCREMENT, "Data" TEXT NOT N
 
 # Insert test data to our database
 
-INSERT INTO "Socis" ("Codi", "DataAlta", "DNI", "Nom", "Cognom1", "Cognom2", "Adreça", "Telefon", "CodiPostal", "Poblacio", "Email", "Pagat") VALUES (100, '20090325', '11223344A', 'Joan', 'Perramon', 'Gràcia', 'Juvenal, 1', '937776655', '08201', 'Sabadell', 'joanperramon@gmail.com', 1);
-INSERT INTO "Socis" ("Codi", "DataAlta", "DNI", "Nom", "Cognom1", "Cognom2", "Adreça", "Telefon", "CodiPostal", "Poblacio", "Email", "Pagat") VALUES (201, '20080421', '23423423A', 'Xavi', 'Fernàndez', 'Jovè', 'Xin xan, 100, 2n 1a', '937776655', '08201', 'Sabadell', 'kasdas_iadf@hotmail.com', 0);
-INSERT INTO "Socis" ("Codi", "DataAlta", "DNI", "Nom", "Cognom1", "Cognom2", "Adreça", "Telefon", "CodiPostal", "Poblacio", "Email", "Pagat") VALUES (202, '20040222', '23423435B', 'Pere', 'Martínez', 'Garcia', 'Crta. Terrassa, 1023', '937776655', '08201', 'Sabadell', '1234lasdkf_alsd@gmail.com', 1);
-INSERT INTO "Socis" ("Codi", "DataAlta", "DNI", "Nom", "Cognom1", "Cognom2", "Adreça", "Telefon", "CodiPostal", "Poblacio", "Email", "Pagat") VALUES (1002, '20060305','X4578456Z', 'Marc', 'Palet', 'Castells', 'Gran Via de les Corts Catalanes, 2002, 2on 5a', '937776655', '08201', 'Badía del Vallès', 'mon@gmail.com', 1);
+INSERT INTO "Socis" ("Codi", "DataAlta", "DNI", "Nom", "Cognom1", "Cognom2", "Adreça", "Telefon", "CodiPostal", "Poblacio", "Email", "Pagat") VALUES (100, '20090325', '11223344A', 'Joan', 'Perramon', 'Gràcia', 'Juvenal, 1', '937776655', '08201', 'Sabadell', 'joanperramon@gmail.com', "true");
+INSERT INTO "Socis" ("Codi", "DataAlta", "DNI", "Nom", "Cognom1", "Cognom2", "Adreça", "Telefon", "CodiPostal", "Poblacio", "Email", "Pagat") VALUES (201, '20080421', '23423423A', 'Xavi', 'Fernàndez', 'Jovè', 'Xin xan, 100, 2n 1a', '937776655', '08201', 'Sabadell', 'kasdas_iadf@hotmail.com', "false");
+INSERT INTO "Socis" ("Codi", "DataAlta", "DNI", "Nom", "Cognom1", "Cognom2", "Adreça", "Telefon", "CodiPostal", "Poblacio", "Email", "Pagat") VALUES (202, '20040222', '23423435B', 'Pere', 'Martínez', 'Garcia', 'Crta. Terrassa, 1023', '937776655', '08201', 'Sabadell', '1234lasdkf_alsd@gmail.com', "true");
+INSERT INTO "Socis" ("Codi", "DataAlta", "DNI", "Nom", "Cognom1", "Cognom2", "Adreça", "Telefon", "CodiPostal", "Poblacio", "Email", "Pagat") VALUES (1002, '20060305','X4578456Z', 'Marc', 'Palet', 'Castells', 'Gran Via de les Corts Catalanes, 2002, 2on 5a', '937776655', '08201', 'Badía del Vallès', 'mon@gmail.com', "true");
 
 INSERT INTO "Cannabis" ("CodiSoci", "Data", "IdPot", "Grams", "Preu") VALUES (100, '20100710', 1, 100, 25.30);
 INSERT INTO "Cannabis" ("CodiSoci", "Data", "IdPot", "Grams", "Preu") VALUES (100, '20121223', 2, 200, 55.30);
